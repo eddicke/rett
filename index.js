fb71ae402d0b39a2f4658dc93fe30581d44eeda9
eddicke <adenekanademola931@gmail.com>
2017-06-05 12:38:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
+var oldMsg = []
 io.on('connection', function(socket) {
+  io.emit("oldMsg", oldMsg);
   socket.on('chat', function(username, message) {
     console.log('message received, sent by: ' + username + ', content: ' + message);
+    oldMsg.push('message received, sent by: ' + username + ', content: ' + message);
     io.emit('chat', username, message);
   });
  socket.on('ademola', function(gme){
